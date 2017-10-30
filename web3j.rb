8f6f63e60a55473e867039ced03d2ea82ffc6184
Conor <conor10@gmail.com>
2017-10-30 16:04:23 +0000

    Updated to web3j 3.0

--- web3j.rb
+++ web3j.rb
@@ -2,9 +2,9 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j/releases/download/v2.3.0/web3j-2.3.0.zip"
+  url "https://github.com/web3j/web3j/releases/download/v3.0.0/web3j-3.0.0.zip"
   # update with: shasum -a 256
-  sha256 "5475f8033ccc2a45f5208b34f5da8692106eefde6e036e9373b79ff6d1d26918"
+  sha256 "05830404e074916fdbd98bcf115c045f9eb2ee6a45afbe5d5e84a71de01e8261"
 
   depends_on :java => "1.7+"
 
