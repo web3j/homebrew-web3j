07b843f14529a9360eee9c342cb9902f58af7443
Conor <conor10@gmail.com>
2017-11-15 16:45:21 +0000

    Updated to web3j 3.1.1

--- web3j.rb
+++ web3j.rb
@@ -4,7 +4,7 @@ class Web3j < Formula
   homepage "https://github.com/web3j/web3j"
   url "https://github.com/web3j/web3j/releases/download/v3.1.0/web3j-3.1.0.zip"
   # update with: shasum -a 256
-  sha256 "e141a3a0945b69788dcee08346b21654409709d05b9c124fbfdd82c1b915eff2"
+  sha256 "bf6b1ae9e6f9cfbcfadad036a2524dace053bd956cfbb9a54cb9dc5a96a90877"
 
   depends_on :java => "1.7+"
 
