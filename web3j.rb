65099717ba2733129a6d9ee0ad4771343647d964
Sam Nazha <sam@blk.io>
2019-01-20 21:28:10 +1100

    Updates for web3j-4.1.1

--- web3j.rb
+++ web3j.rb
@@ -2,9 +2,9 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j/releases/download/v4.0.1/web3j-4.0.1.zip"
+  url "https://github.com/web3j/web3j/releases/download/v4.1.1/web3j-4.1.1.zip"
   # update with: shasum -a 256
-  sha256 "a5e0754094334d4dbe8a3d7f3d0914298f6f79aff75fdb909a256cc56370eed9"
+  sha256 "9e0e0c9d09ff6a606768698ceb6ce96454ab3c2a2f2cb2ab8e95f06515df5fb7"
 
   depends_on :java => "1.7+"
 
