2da991451904f9f17ed92b4c99738098d33bf891
Rousos Alexandros <alexandrosrousos@gmail.com>
2019-08-27 15:12:37 +0100

    Change web3j version to 4.5.0

--- web3j.rb
+++ web3j.rb
@@ -2,9 +2,9 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j/releases/download/v4.4.1/web3j-4.4.1.zip"
+  url "https://github.com/web3j/web3j/releases/download/v4.5.0/web3j-4.5.0.zip"
   # update with: shasum -a 256
-  sha256 "57cbc70923bcb75aed7aa5c7a16420f604764a0e2704a725cb77561a8d9dc446"
+  sha256 "3db3b55871ea03fd587f09765f6f4391e6c4efbdc3e627308feca8632e15e136"
 
   depends_on :java => "1.7+"
 
