3e84a7432785eacb3c7fb8fc4397e9c90cc56bbf
Rousos Alexandros <alexandrosrousos@gmail.com>
2019-09-13 23:46:14 +0100

    Sha256 update

--- web3j.rb
+++ web3j.rb
@@ -4,7 +4,7 @@ class Web3j < Formula
   homepage "https://github.com/web3j/web3j"
   url "https://github.com/web3j/web3j/releases/download/v4.5.0/web3j-4.5.0.zip"
   # update with: shasum -a 256
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "3db3b55871ea03fd587f09765f6f4391e6c4efbdc3e627308feca8632e15e136"
 
   depends_on :java => "1.7+"
 
