1b2d54589fb629d86a9b3410fbd195c10e7fff35
Eve Builder <hi@web3labs.com>
2019-09-13 14:41:16 +0000

    Change web3j version to ${VERSION}

--- web3j.rb
+++ web3j.rb
@@ -2,9 +2,9 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j/releases/download/v4.5.0/web3j-4.5.0.zip"
+  url "https://github.com/web3j/web3j/releases/download/v/web3j-.zip"
   # update with: shasum -a 256
-  sha256 "3db3b55871ea03fd587f09765f6f4391e6c4efbdc3e627308feca8632e15e136"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   depends_on :java => "1.7+"
 
