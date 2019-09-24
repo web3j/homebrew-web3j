a9763908b81504ab8d21e6fe51ec69e226576c71
Antony Denyer <git@antonydenyer.co.uk>
2019-09-24 17:34:42 +0000

    Change web3j version to 4.5.5

--- web3j.rb
+++ web3j.rb
@@ -2,9 +2,9 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j/releases/download/v4.5.4/web3j-4.5.4.zip"
+  url "https://github.com/web3j/web3j/releases/download/v4.5.5/web3j-4.5.5.zip"
   # update with: shasum -a 256
-  sha256 "6502a0fe42929d9bce5ebbeffb45a1f8a53d84931440a57b952fba2c5ecf29fa"
+  sha256 "4965a4ca18a33a9987a25e167f7ecf0957b3fbeef16ff9c69fd2a5f37691992f"
 
   depends_on :java => "1.7+"
 
