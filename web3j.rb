f77c53eddb654d30fc80ab0ee5a5ffba5cb16cbd
Antony Denyer <git@antonydenyer.co.uk>
2019-11-19 09:49:26 +0000

    Change web3j version to 4.5.7

--- web3j.rb
+++ web3j.rb
@@ -4,7 +4,7 @@ class Web3j < Formula
   homepage "https://github.com/web3j/web3j"
   url "https://github.com/web3j/web3j-cli/releases/download/v4.5.7/web3j-4.5.7.zip"
   # update with: shasum -a 256
-  sha256 "6f4e3288fc2314b3e302eaea1242ba4610e96da0092941be1fe3aec614f9bc61"
+  sha256 "b6b8f23718b359577c481b7d5ca2a74b397d0a77180a9bbc6bbd4918a089f50e"
 
   depends_on :java => "1.7+"
 
