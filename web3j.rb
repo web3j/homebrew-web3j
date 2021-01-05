c9d230c3bdff6c910d97c5cf76614bd2e78f132a
SebastianGoendoer <sgoendoer@users.noreply.github.com>
2021-01-05 00:42:07 +0000

    Fixed dependency "java" to "openjdk" (#12)
    Thanks @sgoendoer 👍🏼

--- web3j.rb
+++ web3j.rb
@@ -6,7 +6,7 @@ class Web3j < Formula
   # update with: shasum -a 256
   sha256 "8b635d0b0c1d0623b0a42711418dbd6601b53221143f84d6eb264c2713ffd5d7"
 
-  depends_on :java => "1.7+"
+  depends_on :openjdk => "1.7+"
 
   def install
     prefix.install "lib"
